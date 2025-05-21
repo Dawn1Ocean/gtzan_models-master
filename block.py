e724b12645d8a43b4c96b3af9214ee4f8649d6e9
huangyicong0769 <mcrspsht@outlook.com>
2025-05-21 11:08:13 +0800

    feat: add C2f block

--- block.py
+++ block.py
@@ -112,3 +112,16 @@ class CBS2d(Conv):
         super().__init__(c1, c2, k, s, p, act)
         self.conv = nn.Conv2d(c1, c2, k, s, p, bias=False)
         self.bn = nn.BatchNorm2d(c2)
+
+class C2f(nn.Module):
+    def __init__(self, c1, c2, n=1, shortcut=False, e=0.5):
+        super().__init__()
+        self.c = int(c2*e)
+        self.cv1 = CBS2d(c1, 2 * self.c, 1)
+        self.cv2 = CBS2d((2 + n) * self.c, c2, 1)
+        self.m = nn.ModuleList(Bottleneck(self.c, self.c, k=3, e=1.0, shortcut=shortcut) for _ in range(n))
+
+    def forward(self, x:torch.Tensor) -> torch.Tensor:
+        y = list(self.cv1(x).chunk(2, 1))
+        y.extend(m(y[-1]) for m in self.m)
+        return self.cv2(torch.cat(y, 1))
